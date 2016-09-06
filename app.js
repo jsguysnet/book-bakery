cee94f38c6eafedc73e4d9740fe8339652d915ba
= <github@maikpaulus.com>
2016-09-06 21:16:24 +0200

    added web server startet initializing a crawler

--- app.js
+++ app.js
@@ -2,19 +2,20 @@ let express = require('express');
 let bodyParser = require('body-parser');
 let path = require('path');
 
-let Crawler = require(__dirname + '/lib/Crawler.js');
+let Bookery = require(__dirname + '/lib/Bookery.js');
 
 let app = express();
 let port = 2016;
 
+app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 // route to get a list of all books
 app.get('/list', function (request, response) {
-    let crawler = new Crawler(__dirname + '/books');
-    response.send({
-        test: 123
+    let bookery = new Bookery(__dirname + '/books');
+    bookery.list(function (data) {
+        response.send(data);
     });
 });
 
@@ -36,7 +37,7 @@ app.get('/list/:query', function (request, response) {
 
 // route to catch every other call
 app.get('*', function (request, response) {
-    response.sendFile('public/index.html');
+    response.sendFile(__dirname + '/public/index.html');
 });
 
 // standard express call to start the webserver
