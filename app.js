9af777a729f1134a2ebe7d0a0a4439c5a782d33f
Manuel Schächinger <manuel@schaechinger.com>
2016-09-09 22:09:14 +0200

    updated project

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 let Bookery = require(__dirname + '/lib/Bookery.js');
 
 let bookery = new Bookery(__dirname + '/books');
-bookery.list(function (data) {
-    response.json(data);
-});
+bookery.list();
